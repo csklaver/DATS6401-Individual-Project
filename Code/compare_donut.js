41face892de51dffe2a93ace4cfcb1d98ae896aa
csklaver <54607062+csklaver@users.noreply.github.com>
2020-06-13 19:44:08 -0400

    Add files via upload

---
+++ Code/compare_donut.js
@@ -0,0 +1,213 @@
+$(document).ready(function() {
+    $("#donut_argentina_spending_div").show();
+    $("#donut_australia_spending_div").hide();
+    $("#donut_brazil_spending_div").hide();
+    $("#donut_france_spending_div").hide();
+    $("#donut_germany_spending_div").hide();
+    $("#donut_indonesia_spending_div").hide();
+    $("#donut_italy_spending_div").hide();
+    $("#donut_japan_spending_div").hide();
+    $("#donut_korea_spending_div").hide();
+    $("#donut_mexico_spending_div").hide();
+    $("#donut_russia_spending_div").hide();
+    $("#donut_sa_spending_div").hide();
+    $("#donut_turkey_spending_div").hide();
+    $("#donut_uk_spending_div").hide();
+    $("#donut_us_spending_div").hide();
+});
+
+
+$(document).ready(function(){
+    $('#Dropdown').on('change', function() {
+      if ( this.value == 'Argentina')
+      {
+        $("#donut_argentina_spending_div").fadeIn(1000);
+      }
+      else
+      {
+        $("#donut_argentina_spending_div").hide();
+      }
+    });
+});
+
+$(document).ready(function(){
+    $('#Dropdown').on('change', function() {
+      if ( this.value == 'Australia')
+      {
+        $("#donut_australia_spending_div").fadeIn(1000);
+      }
+      else
+      {
+        $("#donut_australia_spending_div").hide();
+      }
+    });
+});
+
+$(document).ready(function(){
+    $('#Dropdown').on('change', function() {
+      if ( this.value == 'Brazil')
+      {
+        $("#donut_brazil_spending_div").fadeIn(1000);
+      }
+      else
+      {
+        $("#donut_brazil_spending_div").hide();
+      }
+    });
+});
+
+$(document).ready(function(){
+    $('#Dropdown').on('change', function() {
+      if ( this.value == 'France')
+      {
+        $("#donut_france_spending_div").fadeIn(1000);
+      }
+      else
+      {
+        $("#donut_france_spending_div").hide();
+      }
+    });
+});
+
+$(document).ready(function(){
+    $('#Dropdown').on('change', function() {
+      if ( this.value == 'Germany')
+      {
+        $("#donut_germany_spending_div").fadeIn(1000);
+      }
+      else
+      {
+        $("#donut_germany_spending_div").hide();
+      }
+    });
+});
+
+$(document).ready(function(){
+    $('#Dropdown').on('change', function() {
+      if ( this.value == 'Indonesia')
+      {
+        $("#donut_indonesia_spending_div").fadeIn(1000);
+      }
+      else
+      {
+        $("#donut_indonesia_spending_div").hide();
+      }
+    });
+});
+
+$(document).ready(function(){
+  $('#Dropdown').on('change', function() {
+    if ( this.value == 'Italy')
+    {
+      $("#donut_italy_spending_div").fadeIn(1000);
+    }
+    else
+    {
+      $("#donut_italy_spending_div").hide();
+    }
+  });
+});
+
+$(document).ready(function(){
+  $('#Dropdown').on('change', function() {
+    if ( this.value == 'Japan')
+    {
+      $("#donut_japan_spending_div").fadeIn(1000);
+    }
+    else
+    {
+      $("#donut_japan_spending_div").hide();
+    }
+  });
+});
+
+$(document).ready(function(){
+  $('#Dropdown').on('change', function() {
+    if ( this.value == 'Korea, Rep.')
+    {
+      $("#donut_korea_spending_div").fadeIn(1000);
+    }
+    else
+    {
+      $("#donut_korea_spending_div").hide();
+    }
+  });
+});
+
+$(document).ready(function(){
+  $('#Dropdown').on('change', function() {
+    if ( this.value == 'Mexico')
+    {
+      $("#donut_mexico_spending_div").fadeIn(1000);
+    }
+    else
+    {
+      $("#donut_mexico_spending_div").hide();
+    }
+  });
+});
+
+$(document).ready(function(){
+  $('#Dropdown').on('change', function() {
+    if ( this.value == 'Russia')
+    {
+      $("#donut_russia_spending_div").fadeIn(1000);
+    }
+    else
+    {
+      $("#donut_russia_spending_div").hide();
+    }
+  });
+});
+
+$(document).ready(function(){
+  $('#Dropdown').on('change', function() {
+    if ( this.value == 'South Africa')
+    {
+      $("#donut_sa_spending_div").fadeIn(1000);
+    }
+    else
+    {
+      $("#donut_sa_spending_div").hide();
+    }
+  });
+});
+
+$(document).ready(function(){
+  $('#Dropdown').on('change', function() {
+    if ( this.value == 'Turkey')
+    {
+      $("#donut_turkey_spending_div").fadeIn(1000);
+    }
+    else
+    {
+      $("#donut_turkey_spending_div").hide();
+    }
+  });
+});
+
+$(document).ready(function(){
+  $('#Dropdown').on('change', function() {
+    if ( this.value == 'United Kingdom')
+    {
+      $("#donut_uk_spending_div").fadeIn(1000);
+    }
+    else
+    {
+      $("#donut_uk_spending_div").hide();
+    }
+  });
+});
+
+$(document).ready(function(){
+  $('#Dropdown').on('change', function() {
+    if ( this.value == 'United States')
+    {
+      $("#donut_us_spending_div").fadeIn(1000);
+    }
+    else
+    {
+      $("#donut_us_spending_div").hide();
+    }
+  });
+});
